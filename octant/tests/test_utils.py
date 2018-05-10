b09c9497118f67f24dfb8db69dcd32794e7016b9
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-05-10 15:13:16 +0100

    add test_great_circle()

---
+++ octant/tests/test_utils.py
@@ -0,0 +1,11 @@
+"""Test the `utils` submodule"""
+# import numpy as np
+import numpy.testing as npt
+from octant.utils import great_circle
+
+
+def test_great_circle():
+    dist = great_circle(10., 20., 30., 40.)
+    true_dist = 1435334.9068947
+
+    npt.assert_almost_equal(dist, true_dist)
